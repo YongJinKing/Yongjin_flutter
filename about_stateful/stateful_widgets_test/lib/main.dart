d5aa67ed2ea241b9e6f5d28c11d16d504b106d2e
YongJinKing <dydwls120@gmail.com>
2023-02-26 21:07:29 +0900

    update

--- about_stateful/stateful_widgets_test/lib/main.dart
+++ about_stateful/stateful_widgets_test/lib/main.dart
@@ -12,6 +12,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //context는 부모요소에 접근하게 해주는 친구
@@ -26,8 +34,14 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargyTitle(),
+            children: [
+              showTitle ? const MyLargyTitle() : const Text("noting"),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(
+                  Icons.remove_red_eye,
+                ),
+              ),
             ],
           ),
         ),
@@ -36,13 +50,33 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargyTitle extends StatelessWidget {
+class MyLargyTitle extends StatefulWidget {
   const MyLargyTitle({
     super.key,
   });
 
   @override
+  State<MyLargyTitle> createState() => _MyLargyTitleState();
+}
+
+class _MyLargyTitleState extends State<MyLargyTitle> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState(); //부모데이터의 변수를 초기화 할때 사용
+    print("init");
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose(); //무언가를 취소하는 메소드
+    print("dispose");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build");
     return Text(
       'My Largy Title',
       style: TextStyle(
---
+++ nico_clonecording2_pomodoro/clone1/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:clone1/screens/home_screens.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //context는 부모요소에 접근하게 해주는 친구
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
